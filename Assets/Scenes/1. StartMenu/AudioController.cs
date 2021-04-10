a30e5de385416f3e7ab88e03cd79f414f38c4ef8
Ivan <igonzalezespi@gmail.com>
2021-04-10 21:37:10 +0200

    Audio mixer con música de fondo y sonidos en botones

---
+++ Assets/Scenes/1. StartMenu/AudioController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    [SerializeField] private Slider musicSlider, sfxSlider;
+
+    [Range(-80, 0)]
+    [SerializeField] private float defaultMusic, defaultSfx;
+
+    private static readonly string firstPlayPref = "firstPlayPref";
+    private static readonly string musicPref = "musicPref";
+    private static readonly string sfxPref = "sfxPref";
+
+    private bool firstPlayCheck;
+
+    private void Start()
+    {
+        this.firstPlayCheck = PlayerPrefs.GetInt(firstPlayPref, 1) == 1;
+
+        if (this.firstPlayCheck)
+        {
+            PlayerPrefs.SetInt(firstPlayPref, 0);
+
+            this.musicSlider.value = this.defaultMusic;
+            this.sfxSlider.value = this.defaultSfx;
+
+            PlayerPrefs.SetFloat(musicPref, this.musicSlider.value);
+            PlayerPrefs.SetFloat(sfxPref, this.sfxSlider.value);
+        }
+        else
+        {
+            this.musicSlider.value = PlayerPrefs.GetFloat(musicPref);
+            this.sfxSlider.value = PlayerPrefs.GetFloat(sfxPref);
+        }
+    }
+
+    public void SetMusic(float volume)
+    {
+        this.audioMixer.SetFloat("music", volume);
+        PlayerPrefs.SetFloat(musicPref, volume);
+    }
+
+    public void SetSfx(float volume)
+    {
+        this.audioMixer.SetFloat("sfx", volume);
+        PlayerPrefs.SetFloat(sfxPref, volume);
+    }
+}
---
+++ Assets/Scripts/ButtonSfx.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ButtonSfx : MonoBehaviour
+{
+    public void Beep(AudioSource source)
+    {
+        source.Play();
+    }
+}
